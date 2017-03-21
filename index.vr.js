a5b1bab416d70f39d2ca18e382331146657f489f
Christo Oosthuizen <christooosthuizen@Christos-MacBook-Pro-2.local>
2017-03-21 17:00:20 +0000

    add animation

--- index.vr.js
+++ index.vr.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TimerMixin from 'react-timer-mixin';
 
 import {
   AppRegistry,
@@ -6,35 +7,89 @@ import {
   StyleSheet,
   Pano,
   Text,
+  Image,
   View,
   Animated,
   Video
 } from 'react-vr';
 
+
 class reactVR extends React.Component {
   constructor(props) {
-       super(props);
+    super(props);
+    this.state      = {rotation: 0};
+    this.lastUpdate = Date.now();
+    this.rotate     = this.rotate.bind(this);
 
   }
+
+  rotate() {
+    const now       = Date.now();
+    const delta     = now - this.lastUpdate;
+    this.lastUpdate = now;
+    this.setState({rotation: this.state.rotation + 0.2});
+    this.frameHandle = requestAnimationFrame(this.rotate);
+  }
+
+  // mixins: [TimerMixin]
   componentDidMount() {
-
+    setTimeout(() => {
+      this.rotate()
+      console.log('reset view!')
+    }, 5000);
+    setTimeout(() => {
+      this.setState({rotate:0})
+      console.log('reset view!')
+    }, 5000);
   }
+
+  componentWillUnmount() {
+    if (this.frameHandle) {
+      cancelAnimationFrame(this.frameHandle);
+      this.frameHandle = null;
+    }
+  }
+
   render() {
    return (
-     <View>
-       <Pano source={asset('3.jpg')}/>
+     <View >
+       <Pano source={asset('3.jpg')} style={{
+         transform: [
+           {translate: [0, 0, 0]},
+           {rotateY : this.state.rotation},
+           {rotateX : 0}
+         ],
+       }}/>
+       <Image source={asset('ship.png')}
+       style={{
+         height: 3,
+         width: 5,
+         layoutOrigin: [0.5,0.5],
+         paddingLeft: 0.2,
+         paddingRight: 0.2,
+         transform: [{translate: [0, 7.5, -18]}],
+       }}/>
+       <Image source={asset('logo.png')}
+       style={{
+         height: 5,
+         width: 8,
+         layoutOrigin: [0.5,0.5],
+         paddingLeft: 0.2,
+         paddingRight: 0.2,
+         transform: [{translate: [0, 7.5, -15]}],
+       }}/>
        <Text
          style={{
-           backgroundColor: 'rgba(0, 0, 0, 0.1)',
-           fontSize: 0.8,
+           fontSize: 0.6,
            layoutOrigin: [0.5, 0.5],
            paddingLeft: 0.2,
            paddingRight: 0.2,
+           color: '#fff',
            textAlign: 'center',
            textAlignVertical: 'center',
-           transform: [{translate: [0, 4, -15]}],
+           transform: [{translate: [0, 11.5, -15]}],
          }}>
-         centered text on enter (15m 4h)
+         DISCOVER YOUR DESTINATION
        </Text>
      </View>
    );
--- vr/client.js
+++ vr/client.js
@@ -9,6 +9,7 @@ function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'reactVR', parent, {
     // Add custom options here
     enableHotReload:true,
+    cursorVisibility: 'visible'
   });
   vr.render = function() {
     // Any custom behavior you want to perform on each frame goes here
